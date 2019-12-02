8032cb263167261f32338d0b8e4b7b2e50e8a1b7
JeiHO <21228328+jeijei4@users.noreply.github.com>
2019-12-02 04:55:46 -0600

    update2

--- ReinosCenfotecos/src/ui/Main.java
+++ ReinosCenfotecos/src/ui/Main.java
@@ -1,23 +1,12 @@
 package ui;
 
-import java.awt.Cursor;
 import java.io.IOException;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
-import bl.Construccion.FabricadorDeTropas;
-import bl.Construccion.Fachada.Fachada;
-import bl.Construccion.Juego.Juego;
-import bl.Construccion.Jugadores.Jugador;
-import bl.Construccion.Tropa.TropaAtaque.Arquero;
-import bl.Construccion.Tropa.TropaAtaque.TropaAtaque;
 import ui.contenedor.FrmMain;
 import ui.contenedor.OSUtil;
 
 public class Main {
-	private static FrmMain elMain;
-
-	private static Fachada fachada = new Fachada();
-	private static Juego juego;
 
 	public static void main(String args[]) throws IOException {
 		try {
@@ -42,90 +31,10 @@ public class Main {
 		java.awt.EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
-				iniciar();
+				FrmMain elMain = new FrmMain();
+				elMain.setVisible(true);
+				elMain.iniciar();
 			}
 		});
-	}
-
-	private static void iniciar() {
-
-		elMain = new FrmMain();
-		elMain.setVisible(true);
-
-		// TODO: Crear panel donde selecciona la cantidad de jugadores y se escribe el
-		// nombre de cada jugador:
-		int cantidadJugadores = 3;
-
-		if (cantidadJugadores < 5) {
-
-			/*System.out.println(FabricadorDeTropas.procesarFuncion(1));
-			System.out.println(FabricadorDeTropas.procesarFuncion(2));
-			System.out.println(FabricadorDeTropas.procesarFuncion(3));
-			System.out.println(FabricadorDeTropas.procesarFuncion(4));
-			System.out.println(FabricadorDeTropas.procesarFuncion(5));
-			System.out.println(FabricadorDeTropas.procesarFuncion(6));
-			System.out.println(FabricadorDeTropas.procesarFuncion(7));
-			System.out.println(FabricadorDeTropas.procesarFuncion(8));
-			System.out.println(FabricadorDeTropas.procesarFuncion(9));
-			System.out.println(FabricadorDeTropas.procesarFuncion(10));*/
-			
-			FabricadorDeTropas.procesarFuncion(1);
-			FabricadorDeTropas.procesarFuncion(2);
-			FabricadorDeTropas.procesarFuncion(3);
-			FabricadorDeTropas.procesarFuncion(4);
-			FabricadorDeTropas.procesarFuncion(5);
-			FabricadorDeTropas.procesarFuncion(6);
-			FabricadorDeTropas.procesarFuncion(7);
-			FabricadorDeTropas.procesarFuncion(8);
-			FabricadorDeTropas.procesarFuncion(9);
-			FabricadorDeTropas.procesarFuncion(10);
-
-			juego = fachada.construirJuego(cantidadJugadores);
-
-			if (null != juego) {
-
-				elMain.setTableroUI(juego.getTablero());
-
-				for (Jugador jugador : juego.getJugadores()) {
-					jugador.setNombreJugador("Jugador"+cantidadJugadores);
-					--cantidadJugadores;
-					System.out.println(jugador.getNombreJugador());
-				}
-
-				// Mostrar el tablero:
-				elMain.mostrarTablero();
-
-				TropaAtaque pieza = new Arquero();
-				juego.getJugadores().get(0).getTropas().add(pieza);
-
-				// Ejemplo:
-				juego.getTablero().getCasillas()[0][1].setPieza(pieza);
-				System.out.println(juego.getTablero().recorrerTablero());
-
-				// Pintar casillas que no están vacias:
-				for (int i = 0; i < juego.getTablero().getAncho(); i++) {
-					for (int j = 0; j < juego.getTablero().getLargo(); j++) {
-						if (juego.getTablero().getCasillas()[i][j].getPieza() != null) {
-							String nombrePieza = juego.getTablero().getCasillas()[i][j].getPieza().getNombre();
-							elMain.getTableroUI().construirEnCasilla(i, j, nombrePieza);
-
-						}
-					}
-				}
-
-			} else {
-				elMain.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-				elMain.setVisible(false);
-				elMain.dispose();
-				System.err.println("No está permitido un jugador");
-				System.exit(0);
-			}
-		} else {
-			elMain.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-			elMain.setVisible(false);
-			elMain.dispose();
-			System.err.println("No está permitido más de 4 jugadores");
-			System.exit(0);
-		}
 	}
 }
